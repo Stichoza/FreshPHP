12327254f3af6a68cbb984d1d1ade4f8d6eabcdc
Stichoza <stichoza@gmail.com>
2013-11-21 03:14:37 +0400

    Singleton interface

---
+++ src/FreshPHP/Singleton/ISingleton.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace FreshPHP\Singleton;
+
+/**
+ * Interface ISingleton
+ * @package FreshPHP\Singleton
+ * @author Stichoza <me@stichoza.com>
+ */
+interface ISingleton {
+
+    public function getInstance();
+
+} 
