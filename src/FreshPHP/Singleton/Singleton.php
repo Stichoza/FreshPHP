48123d8729b4a3c9e0f06e24201d755545dfeaca
Stichoza <stichoza@gmail.com>
2013-11-23 21:28:41 +0400

    undefined index fix

--- src/FreshPHP/Singleton/Singleton.php
+++ src/FreshPHP/Singleton/Singleton.php
@@ -4,15 +4,22 @@ namespace FreshPHP\Singleton;
 
 abstract class Singleton implements ISingleton {
 
+    /**
+     * @var array Instance container
+     */
     private static $instances = array();
 
+    /**
+     * Get instance of self
+     * @return Singleton|mixed class instance
+     */
     public static final function getInstance() {
         $class = get_called_class();
-        if (empty(self::$instances[$class])) {
+        if (!isset(self::$instances[$class]) || !is_object(self::$instances[$class])) {
             $rc = new \ReflectionClass($class);
             self::$instances[$class] = $rc->newInstanceArgs(func_get_args());
         }
         return self::$instances[$class];
     }
 
-} 
+}
