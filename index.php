b1f97a81fb74b4d8d69a43d265c42fe996c6152b
Stichoza <stichoza@gmail.com>
2013-11-25 06:01:49 +0400

    update

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-
-require_once "src/FreshPHP/Igniter.php";
-
-new FreshPHP\Igniter();
+require_once "src/FreshPHP/Config/ClassAutoloader.php";
+FreshPHP\Config\ClassAutoloader::register();
 
 FreshPHP\MVC\MVCRouter::getController()->main();
