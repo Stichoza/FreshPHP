39da0d59045bdb848260d6fa2df9a1a212644037
Stichoza <stichoza@gmail.com>
2013-11-26 22:05:48 +0400

    block comments

--- src/FreshPHP/Database/PDOWrapper/Exception/PDOWrapperException.php
+++ src/FreshPHP/Database/PDOWrapper/Exception/PDOWrapperException.php
@@ -2,7 +2,12 @@
 
 namespace FreshPHP\Database\PDOWrapper\Exception;
 
-
+/**
+ * Class PDOWrapperException
+ * @package FreshPHP\Database\PDOWrapper\Exception
+ * @author Stichoza <me@stichoza.com>
+ */
 class PDOWrapperException extends \Exception {
+    // or extends PDOException?
 
-} 
+}
