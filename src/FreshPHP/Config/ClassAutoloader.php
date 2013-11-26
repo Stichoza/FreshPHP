e77abdc3477bcdd91925c89edfb18c141bef77f1
Stichoza <stichoza@gmail.com>
2013-11-26 04:24:24 +0400

    try-catch in register is_readable() in loader

--- src/FreshPHP/Config/ClassAutoloader.php
+++ src/FreshPHP/Config/ClassAutoloader.php
@@ -11,10 +11,24 @@ class ClassAutoloader {
 
     const CLASSPATH = "src";
 
+    /**
+     * Register class autoloader
+     * @return bool
+     */
     public static function register() {
-        spl_autoload_register(__NAMESPACE__ . '\\ClassAutoloader::classAutoload');
+        try {
+            $loader = spl_autoload_register(__NAMESPACE__ . '\\ClassAutoloader::classAutoload', true, true);
+        } catch (\Exception $e) {
+            $loader = false;
+        }
+        return $loader;
     }
 
+    /**
+     * Autoloader method
+     * @param $className
+     * @throws \Exception
+     */
     public static function classAutoload($className) {
         $filename = self::CLASSPATH;
         foreach (explode("\\", $className) as $part) {
@@ -23,8 +37,11 @@ class ClassAutoloader {
         $filename .= ".php";
         if (!file_exists($filename)) {
             throw new \Exception("Class file does not exist ($filename)");
+        } elseif (!is_readable($filename)) {
+            throw new \Exception("Class file is not readable ($filename)");
+        } else {
+            require_once $filename;
         }
-        require_once $filename;
     }
 
 } 
