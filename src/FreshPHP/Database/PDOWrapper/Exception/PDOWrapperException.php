d87aa2e652b046cc8c5809265956446de5495ca1
Stichoza <stichoza@gmail.com>
2013-11-26 05:19:43 +0400

    throwable

--- src/FreshPHP/Database/PDOWrapper/Exception/PDOWrapperException.php
+++ src/FreshPHP/Database/PDOWrapper/Exception/PDOWrapperException.php
@@ -1,14 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Stichoza
- * Date: 26/11/13
- * Time: 05:17
- */
 
 namespace FreshPHP\Database\PDOWrapper\Exception;
 
 
-class PDOWrapperException {
+class PDOWrapperException extends \Exception {
 
 } 
