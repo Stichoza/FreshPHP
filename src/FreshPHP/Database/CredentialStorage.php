764c408b6e6847ae7cdee98b9cf1376ff08d45f1
Stichoza <stichoza@gmail.com>
2013-11-28 02:31:21 +0400

    add class CredentialStorage

---
+++ src/FreshPHP/Database/CredentialStorage.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace FreshPHP\Database;
+
+/**
+ * Class CredentialStorage
+ * @package FreshPHP\Database
+ * @author Stichoza <me@stichoza.com>
+ */
+class CredentialStorage {
+
+    const DB_HOST = "localhost";
+
+    const DB_USER = "root";
+
+    const DB_PASS = "";
+
+    const DB_NAME = "db1";
+
+} 
