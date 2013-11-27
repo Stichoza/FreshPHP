75e8a5d3f620055df51e3942ff764399584dce79
Stichoza <stichoza@gmail.com>
2013-11-28 01:50:38 +0400

    running fw

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 require_once "src/FreshPHP/Config/ClassAutoloader.php";
-FreshPHP\Config\ClassAutoloader::register();
 
-FreshPHP\MVC\MVCRouter::getController()->main();
+use FreshPHP\HTTP\Request;
+use FreshPHP\MVC\MVCRouter;
+
+FreshPHP\Config\ClassAutoloader::register() || die("Cannot register autoloader");
+
+try {
+    MVCRouter::getController()->main();
+} catch (Exception $e) {
+    trigger_error(get_class($e), E_USER_ERROR);
+}
+
+exit();
