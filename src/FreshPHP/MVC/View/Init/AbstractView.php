ad3f6fe313dc99fbe7a18732fc624330cd51ce77
Stichoza <stichoza@gmail.com>
2013-11-28 03:13:14 +0400

    method implementations

--- src/FreshPHP/MVC/View/Init/AbstractView.php
+++ src/FreshPHP/MVC/View/Init/AbstractView.php
@@ -2,11 +2,29 @@
 
 namespace FreshPHP\View\Init;
 
+use FreshPHP\View\Init\ViewParameterStorage as V;
+
 /**
  * Class AbstractView
  * @package FreshPHP\View\Init
  * @author Stichoza <me@stichoza.com>
  */
 abstract class AbstractView {
+
+    public function setParam($key, $value) {
+        try {
+            V::set($key, $value);
+        } catch (\Exception $e) {
+            throw $e;
+        }
+    }
+
+    public function getParam($key, $default = null) {
+        return V::get($key, $default);
+    }
+
+    public function loadFile($filename) {
+        include "res/html/" . $filename; // TODO output contents of non-php files
+    }
 
 } 
