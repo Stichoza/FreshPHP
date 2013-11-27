597bd1e8c133e14c562394faeb1d33bb6ee4b0fe
Stichoza <stichoza@gmail.com>
2013-11-27 18:41:54 +0400

    routing indexes

--- src/FreshPHP/MVC/MVCRouter.php
+++ src/FreshPHP/MVC/MVCRouter.php
@@ -4,6 +4,9 @@ namespace FreshPHP\MVC;
 
 use FreshPHP\Config\ConfigFileHandler;
 use FreshPHP\HTTP\Request;
+use FreshPHP\MVC\Exception\InvalidControllerException;
+use FreshPHP\MVC\Exception\NoIndexRouteException;
+use FreshPHP\MVC\Exception\UndefinedControllerException;
 
 /**
  * Class MVCRouter
@@ -15,8 +18,10 @@ class MVCRouter {
     /**
      * Get controller based on HTTP request
      * @param array $args Controller arguments
-     * @return mixed
-     * @throws \Exception
+     * @return object Controller
+     * @throws Exception\UndefinedControllerException
+     * @throws Exception\InvalidControllerException
+     * @throws Exception\NoIndexRouteException
      */
     public static function getController(array $args = array()) {
 
@@ -24,14 +29,39 @@ class MVCRouter {
         $mvc = $config->getParam("framework", "mvc");
         $requestArray = Request::getDirArray();
 
-        // TODO Route request here (get name of controller)
-        $controllerClassName = "DemoController";
+        $requestArray = $args;
+
+        $routePath = $mvc["route"];
+        $controllerClassName = null;
+
+        for ($i = $mvc["start_index"]; $i < count($requestArray); $i++) {
+            if ($i == $mvc["start_index"]
+                && isset($requestArray[$i])
+                && empty($requestArray[$i])) {
+                $controllerClassName = $routePath["."];
+                break;
+            } else if (!isset($routePath[$requestArray[$i]])) {
+                throw new UndefinedControllerException();
+            } else if (!isset($requestArray[$i+1])
+                || empty($requestArray[$i+1])) {
+                if (isset($routePath[$requestArray[$i]]["."])) {
+                    $controllerClassName = $routePath[$requestArray[$i]]["."];
+                } else {
+                    throw new NoIndexRouteException();
+                }
+                break;
+            } else {
+                $routePath = $routePath[$requestArray[$i]];
+            }
+        } // TODO Check this
+
+        return $controllerClassName;
 
         $controllerReflection = new \ReflectionClass(__NAMESPACE__ . "\\Controller\\" . $controllerClassName);
         if ($controllerReflection->implementsInterface(__NAMESPACE__ . "\\Controller\\Init\\IController")) {
             return $controllerReflection->newInstance($args);
         } else {
-            throw new \Exception("Controller is not implementing interface IController");
+            throw new InvalidControllerException("Controller is not implementing interface IController");
         }
 
     }
