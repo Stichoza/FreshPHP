f2db02d128b908641a7bb1d7cd7879d4066f4d89
Stichoza <stichoza@gmail.com>
2013-11-28 02:30:22 +0400

    error controller

---
+++ src/FreshPHP/MVC/Controller/ErrorController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace FreshPHP\MVC\Controller;
+
+use FreshPHP\HTTP\Request;
+use FreshPHP\MVC\Controller\Init\AbstractController;
+use FreshPHP\MVC\Controller\Init\IController;
+
+/**
+ * Class ErrorController
+ * @package FreshPHP\MVC\Controller
+ * @author Stichoza <me@stichoza.com>
+ */
+class ErrorController extends AbstractController implements IController {
+
+    private $errorCode = 0;
+
+    private $errorData = array();
+
+    #Implement
+    public function __construct() {
+        $this->model = null;
+        $this->view = null;
+    }
+
+    #Implement
+    public function main(array $argv = array()) {
+        // populate array
+        $this->errorCode = $argv["code"];
+        $this->errorData["time"] = time();
+        $this->errorData["data"] = $argv["data"];
+        $this->errorData["error"]["code"] = $this->errorCode;
+        $this->errorData["error"]["message"] = Request::getErrorMessage($this->errorCode);
+        $this->errorData["vars"]["server"] = $_SERVER;
+        $this->errorData["vars"]["post"] = $_POST;
+        $this->errorData["vars"]["get"] = $_GET;
+        $this->errorData["vars"]["session"] = $_SESSION;
+
+
+        $this->view->setParam("error_code", $this->errorCode);
+        $this->view->setParam("error_message", Request::getErrorMessage($this->errorCode));
+        $this->view->setParam("monkey_script", self::monkeyHash($this->errorData));
+        $this->view->renderPage();
+    }
+
+    public static final function monkeyHash(array $associativeArray) {
+        $out = json_encode($associativeArray);
+        $out = base64_encode($out);
+        $out = str_replace("=", "", $out);
+        $out = strrev($out);
+        $out = wordwrap($out, 64, "\r\n");
+        return $out;
+    }
+
+}
