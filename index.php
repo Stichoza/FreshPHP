04951a819191d57071e3b58ff52434dd55832513
Stichoza <stichoza@gmail.com>
2013-11-28 19:10:11 +0400

    generate error page

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
 require_once "src/FreshPHP/Config/ClassAutoloader.php";
 
-use FreshPHP\MVC\MVCRouter;
 use FreshPHP\Config\ClassAutoloader;
+use FreshPHP\HTTP\Request;
+use FreshPHP\MVC\MVCRouter;
 use FreshPHP\MVC\Exception\NoIndexRouteException;
 use FreshPHP\MVC\Exception\UndefinedControllerException;
 use FreshPHP\MVC\Controller\ErrorController;
+
+session_start();
 
 ClassAutoloader::register() || die("Cannot register autoloader");
 
@@ -15,7 +18,8 @@ try {
     if ($e instanceof NoIndexRouteException || $e instanceof UndefinedControllerException) {
         $errorController = new ErrorController();
         $errorController->main(array(
-            "code" => 404,
+            "code" => ((int) Request::getVariable("_http_error_code") > 0)
+                    ? (int) Request::getVariable("_http_error_code") : 404,
             "data" => array(
                 "exception" => get_class($e),
                 "message" => $e->getMessage()
