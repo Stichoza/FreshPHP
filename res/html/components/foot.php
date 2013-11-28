6af099b7d21c3286084769f12c68e3577d35791c
Stichoza <stichoza@gmail.com>
2013-11-28 20:34:27 +0400

    split files

---
+++ res/html/components/foot.php
@@ -0,0 +1,12 @@
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="/lib/jquery/1.10.1/jquery-1.10.1.min.js"><\/script>')</script>
+		<script src="/lib/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+		<script src="/res/js/main.js"></script>
+		<script>
+			var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+			(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+			g.src='//www.google-analytics.com/ga.js';
+			s.parentNode.insertBefore(g,s)}(document,'script'));
+		</script>
+	</body>
+</html>
