0f95f17106da04b279f6325c6c36820e67e91655
Stichoza <stichoza@gmail.com>
2013-11-28 04:35:23 +0400

    catch all the exceptions

--- index.php
+++ index.php
@@ -3,15 +3,28 @@ require_once "src/FreshPHP/Config/ClassAutoloader.php";
 
 use FreshPHP\MVC\MVCRouter;
 use FreshPHP\Config\ClassAutoloader;
+use FreshPHP\MVC\Exception\NoIndexRouteException;
+use FreshPHP\MVC\Exception\UndefinedControllerException;
+use FreshPHP\MVC\Controller\ErrorController;
 
 ClassAutoloader::register() || die("Cannot register autoloader");
 
 try {
     MVCRouter::getController()->main();
 } catch (Exception $e) {
-    $exceptionMessage = $e->getMessage();
-    trigger_error(get_class($e) . ((empty($exceptionMessage))
-        ? "" : " - " . $exceptionMessage), E_USER_ERROR);
+    if ($e instanceof NoIndexRouteException || $e instanceof UndefinedControllerException) {
+        $errorController = new ErrorController();
+        $errorController->main(array(
+            "code" => 404,
+            "data" => array(
+                "exception" => get_class($e),
+                "message" => $e->getMessage()
+            )
+        ));
+    } else {
+        $exceptionMessage = $e->getMessage();
+        trigger_error(get_class($e) . ((empty($exceptionMessage))
+                ? "" : " - " . $exceptionMessage), E_USER_ERROR);
+    }
 }
-
-exit();
+exit;
