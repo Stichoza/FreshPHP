100c3501d1e079f86501eb8c10874dca54ba5cb8
Stichoza <stichoza@gmail.com>
2013-11-28 19:40:14 +0400

    split components

--- src/FreshPHP/MVC/View/DemoView.php
+++ src/FreshPHP/MVC/View/DemoView.php
@@ -7,7 +7,9 @@ use FreshPHP\MVC\View\Init\AbstractView;
 class DemoView extends AbstractView {
 
     public function render() {
+        $this->loadFile("components/head.php");
         $this->loadFile("demo.html");
+        $this->loadFile("components/foot.php");
     }
 
 } 
