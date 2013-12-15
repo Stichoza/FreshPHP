809d55e3ea224c59f8b382c4c61a44b32ca9eca8
Stichoza <stichoza@gmail.com>
2013-12-15 05:44:12 +0400

    Jaro-Winkler algorithm init

---
+++ src/FreshPHP/Text/JaroWinkler/JaroWinkler.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace FreshPHP\Text\JaroWinkler;
+
+/**
+ * Class JaroWinkler
+ * @package FreshPHP\Text\JaroWinkler
+ * @author Stichoza <me@stichoza.com>
+ */
+class JaroWinkler {
+
+    /**
+     * @var String one
+     */
+    protected $string1;
+
+    /**
+     * @var String two
+     */
+    protected $string2;
+
+
+
+} 
